42024881af1f37faa1bb2f46667db51dc9b45049
Alan Wilhelm <alanwilhelm@gmail.com>
2017-06-11 15:50:12 -0700

    add net lib

--- lib/exthereum.ex
+++ lib/exthereum.ex
@@ -1,6 +1,7 @@
 defmodule Exthereum do
   alias Exthereum.Web3
   alias Exthereum.Eth
+  alias Exthereum.Net
   @moduledoc """
   Documentation for Exthereum.
   """
@@ -10,16 +11,32 @@ defmodule Exthereum do
 
   ## Examples
 
-      iex> Exthereum.get_version
+      iex> Exthereum.client_version
       {:ok, "Geth/v1.6.5-stable-cf87713d/darwin-amd64/go1.8.3"}
+
+      iex> Exthereum.sha3("0x68656c6c6f20776f726c64")
+      {:ok, "47173285a8d7341e5e972fc677286384f802f8ef42a5ec5f03bbfa254cb01fad"}
+
+      iex> Exthereum.net_version
+      {:ok, "1"}
 
       iex> Exthereum.get_balance("0xfE8bf4ca8A6170E759E89EDB5cc9adec3e33493f") # Donations accepted :-)
       {:ok, 0.4650075166583676}
 
   """
-  @spec get_version :: {:ok, String.t} | {:error, String.t}
-  def get_version do
-    Web3.get_version
+  @spec client_version :: {:ok, String.t} | {:error, String.t}
+  def client_version do
+    Web3.client_version
+  end
+
+  @spec sha3(String.t) :: {:ok, String.t} | {:error, String.t}
+  def sha3(str_to_hash) do
+    Web3.sha3(str_to_hash)
+  end
+
+  @spec net_version :: {:ok, String.t} | {:error, String.t}
+  def net_version do
+    Net.version
   end
 
   @spec get_balance(String.t) :: {:ok, float} | {:error, String.t}
--- lib/exthereum/rpc/web3.ex
+++ lib/exthereum/rpc/web3.ex
@@ -1,8 +1,8 @@
 defmodule Exthereum.Web3 do
   use Exthereum.Transport
 
-  @spec get_version :: {:ok, String.t} | {:error, String.t}
-  def get_version do
+  @spec client_version :: {:ok, String.t} | {:error, String.t}
+  def client_version do
     case send("web3_clientVersion") do
       {:ok, version} ->
         {:ok, version}
@@ -10,5 +10,17 @@ defmodule Exthereum.Web3 do
         {:error, reason}
     end
   end
+
+  @spec sha3(String.t) :: {:ok, float} | {:error, String.t}
+  def sha3(str) do
+    case __MODULE__.send("web3_sha3",[str]) do
+      {:ok, sha} ->
+        {:ok, sha}
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+
+  #curl -X POST --data '{"jsonrpc":"2.0","method":"web3_sha3","params":["0x68656c6c6f20776f726c64"],"id":64}' localhost:8545
 
 end
