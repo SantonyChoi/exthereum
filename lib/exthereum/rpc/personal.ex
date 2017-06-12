b663e7af69879dc6e5df2e41cb5a13dc4c134c91
Alan Wilhelm <alanwilhelm@gmail.com>
2017-06-11 23:26:04 -0700

    (ﾉ◕ヮ◕)ﾉ*:･✧ Run CI ✧

--- lib/exthereum/rpc/personal.ex
+++ lib/exthereum/rpc/personal.ex
@@ -47,9 +47,8 @@ but I am doing it anyway.
       "to": to,
       "gas": "0x76c0", # 30400,
       #"gasPrice": "0x9184e72a000", # 10000000000000
-      #"value": "0x19788FD6F800", # 2441406250
-      "value": hex_wei_value, # 2441406250
-      "data": "0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675"}, password]
+      "value": hex_wei_value,
+      "data": "0x0"}, password]
 
     case __MODULE__.send("personal_sendTransaction", params) do
       {:ok, result} ->
